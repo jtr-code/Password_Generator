7780163c8400083ad09b0d702e01c969c572952d
jtr-code <jtrtricks@gmail.com>
2022-11-08 00:16:30 +0530

    Added all functionality except copy

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+
 class Password {
-
     generateSuperStrongPassword(length) {
-
         let result = '';
         let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_';
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
+        let charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
         return result;
+
     }
     generateStrongPassword(length) {
 
         let resultStrong = '';
         let charactersForStrong = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        var charactersLength = charactersForStrong.length;
-        for (var i = 0; i < length; i++) {
+        let charactersLength = charactersForStrong.length;
+        for (let i = 0; i < length; i++) {
             resultStrong += charactersForStrong.charAt(Math.floor(Math.random() * charactersLength));
         }
         return resultStrong;
@@ -24,13 +24,35 @@ class Password {
 
         let charactersForWeak = ["123456", '123456789', "qwerty", "password", "12345", "12345678", "111111", "1234567", "123123", "qwerty123", "1q2w3e", "1234567890", "DEFAULT", "000000", "abc123", "654321", "123321", "qwertyuiop", "Iloveyou", "666666", "jishnu", "myname", "arjun", "ajay", "anurag", "mathew", "rohit"];
         const random = charactersForWeak[(Math.random() * charactersForWeak.length) | 0];
-        console.log(random);
+        console.log(random)
+        return random;
     }
-
-
 }
 
-let passwordObj = new Password();
+let passwordDisplay = document.getElementById('passworddisplay');
+let generateBtn = document.getElementById('generatebtn');
+let radio = document.getElementsByClassName("radiobtn");
+// function myFunction() {
+//     document.getElementById("passworddisplay").innerHTML = passwordObj.generateSuperStrongPassword(14);
+// }
+
+let passwordObj = new Password(); 
+
+generateBtn.addEventListener("click", (e) => {
+    let pass;
+    e.preventDefault();
+    if (radio[0].checked) {
+        pass = passwordObj.generateSuperStrongPassword(16);
+    } else if (radio[1].checked) {
+        pass = passwordObj.generateStrongPassword(10);
+
+    } else if (radio[2].checked) {
+        pass = passwordObj.generateWeakPassword();
+    }
+    passwordDisplay.innerHTML = pass;
+})
+
+
 console.log(passwordObj.generateSuperStrongPassword(14))
 console.log(passwordObj.generateStrongPassword(10))
-passwordObj.generateWeakPassword()
+passwordObj.generateWeakPassword();
